191e3225ee84287411b31238c238c0e5edb00bde
Monlo <lopezmonserrat.14@gmail.com>
2024-06-02 13:40:00 +0200

    changes_workshopspage

--- scrript.js
+++ scrript.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
     // Smooth scroll for the navigation menu and links with .scrollto classes
     $('.nav-link, .scrollto').click(function(e) {
-      e.preventDefault();
-      var target = $(this).attr('href');
-      $('html, body').animate({
-        scrollTop: $(target).offset().top
-      }, 1000, 'easeInOutExpo');
+        e.preventDefault();
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 1000, 'easeInOutExpo');
     });
-  
+
     // Activate scrollspy to add active class to navbar items on scroll
     $('body').scrollspy({
-      target: '#navbar',
-      offset: 80
+        target: '#navbar',
+        offset: 80
     });
-  
-    // Collapse Navbar
+
+    // Function to collapse Navbar
     var navbarCollapse = function() {
-      if ($("#navbar").offset().top > 100) {
-        $("#navbar").addClass("navbar-shrink");
-      } else {
-        $("#navbar").removeClass("navbar-shrink");
-      }
+        if ($("#navbar").offset().top > 100) {
+            $("#navbar").addClass("navbar-shrink");
+        } else {
+            $("#navbar").removeClass("navbar-shrink");
+        }
     };
-  
+
     // Collapse now if page is not at top
     navbarCollapse();
-    
+
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
-  });
+});
